668cd501e20a2a361374b801d1e1e77bd7ffda79
Stephane <stephanealpha@icloud.com>
2025-06-19 16:04:45 +0200

    Update default.sh

--- derivatives/pytorch/derivatives/a1111/provisioning_scripts/default.sh
+++ derivatives/pytorch/derivatives/a1111/provisioning_scripts/default.sh
@@ -15,7 +15,10 @@ PIP_PACKAGES=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://civitai.com/api/download/models/798204" "https://civitai.com/api/download/models/1838857" "https://civitai.com/api/download/models/1899516" "https://civitai.com/api/download/models/1906687"
+    "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
+    "https://civitai.com/api/download/models/1838857?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+    "https://civitai.com/api/download/models/1899516?type=Model&format=SafeTensor&size=pruned&fp=fp16" 
+    "https://civitai.com/api/download/models/1906687?type=Model&format=SafeTensor&size=pruned&fp=fp16"
 )
 
 UNET_MODELS=(
